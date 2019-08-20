3a16881eda85fbc3665cf0b158b03e3430cd2a73
Екатерина Гавренко <kategavua@gmail.com>
2019-08-20 10:49:34 +0100

    Create a header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from React;
+
+export default function Header() {
+    return(
+        <header>
+            <ul>
+                <li>About me</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+}
