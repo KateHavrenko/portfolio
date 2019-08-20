881687eed7be4386b7bcbc0ec809617b641f2469
Екатерина Гавренко <kategavua@gmail.com>
2019-08-20 10:55:24 +0100

    Fix header component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header></Header>
     </div>
   );
 }
